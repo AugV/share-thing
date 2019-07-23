ed75461745d5742b7c331d1ae7b8ceb7ca40355f
Vainius Augustinas <augustinas.vainius@tieto.com>
2019-07-23 16:20:21 +0300

    dependencies updated

--- sharething_pwa/src/App.test.js
+++ sharething_pwa/src/App.test.js
@@ -4,6 +4,12 @@ import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render( < App / > , div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+describe('Addition', () => {
+  it('knows that 2 and 2 make 4', () => {
+    expect(2 + 2).toBe(4);
+  });
+})
