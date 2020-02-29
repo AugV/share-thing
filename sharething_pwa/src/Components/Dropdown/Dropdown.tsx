c4e0927ec66f38fcc0fe92792c3d429bdb9ec086
unknown <augustinas.vainius@gmail.com>
2020-02-29 21:58:38 +0200

    Header dropdown complete

---
+++ sharething_pwa/src/Components/Dropdown/Dropdown.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './dropdowns.css';
+import { Link, useLocation } from 'react-router-dom';
+
+interface DropDownProps {
+    selectionItems: Map<string, string>;
+}
+
+const DropdownComponent: React.FC<DropDownProps> = (props) => {
+    const linkList = Array.from(props.selectionItems, ([key, value]) => ({ route: key, name: value }));
+
+    const location = useLocation();
+
+    return(
+        <div className="dropdown">
+            <button className="dropbtn">
+                <span style={{ fontSize: '20px' }}>&#x25bc;</span>
+                {props.selectionItems.get(location.pathname)}
+            </button>
+            <div className="dropdown-content">
+            {
+            linkList.map((value, key) => (
+                <Link className={'item'} key={key} to={value.route}>{value.name}</Link>
+            ))
+            }
+            </div>
+        </div>
+    );
+};
+
+export const Dropdown = DropdownComponent;
--- sharething_pwa/src/Components/headers/HomeHeader.tsx
+++ sharething_pwa/src/Components/headers/HomeHeader.tsx
@@ -1,60 +1,25 @@
 import React from 'react';
 import './headers.css';
-import { Dropdown } from 'react-bootstrap';
+import { Dropdown } from '../Dropdown/Dropdown';
+import * as ROUTES from '../../Constants/Routes';
 
-const CustomToggle = React.forwardRef(({ children, onClick }: any, ref: any) => (
-    <h4
-        style={{ width: '50%' }}
-        ref={ref}
-        onClick={e => {
-            e.preventDefault();
-            onClick(e);
-        }}
-    >
-    &#x25bc;
-    {children}
-    </h4>
-  ));
+const HomeHeaderComponent: React.FC<any> = () => {
 
-const CustomMenu = React.forwardRef(
-  ({ children, style, className }: any, ref: any) => {
+    const selectionItems: Map<string, string> = getMapForDropdown();
 
-      return (
-      <div
-        ref={ref}
-        style={style}
-        className={className}
-      >
+    function getMapForDropdown(): Map<string, string> {
+        const map: Map<string, string> = new Map<string, string>();
+        map.set(ROUTES.MY_ITEMS, 'My Items');
+        map.set(ROUTES.PUBLIC, 'Lent Items');
+        map.set(ROUTES.CONVO_LIST, 'Borrowed Items');
+        return map;
+    }
 
-        <ul className="list-unstyled">
-          {React.Children.toArray(children).filter(
-            child =>
-            // @ts-ignore
-              child.props.children.toLowerCase(),
-          )}
-        </ul>
-      </div>
-      );
-  },
-);
-
-const HomeHeaderComponent: React.FC<any> = () => (
+    return(
     <div className="home-header">
-    <Dropdown>
-    <Dropdown.Toggle as={CustomToggle} id="dropdown-custom-components">
-      Custom toggle
-    </Dropdown.Toggle>
-
-    <Dropdown.Menu  as={CustomMenu} >
-      <Dropdown.Item eventKey="1">Red</Dropdown.Item>
-      <Dropdown.Item eventKey="2">Blue</Dropdown.Item>
-      <Dropdown.Item eventKey="3" active={true}>
-        Orange
-      </Dropdown.Item>
-      <Dropdown.Item eventKey="1">Red-Orange</Dropdown.Item>
-    </Dropdown.Menu>
-  </Dropdown>,
+      <Dropdown selectionItems={selectionItems}/>
     </div>
-);
+    );
+};
 
 export const HomeHeader = HomeHeaderComponent;
